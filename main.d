4a1232bf0d7abaa61f1552d2b709fd7b8d6d5b18
1AntonioVP3 <80290410+1AntonioVP3@users.noreply.github.com>
2021-10-22 23:41:52 -0500

    PROYECTO

---
+++ main.d
@@ -0,0 +1,7 @@
+.\main.o: main.c
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\main.o: tm4c123gh6pm.h
+.\main.o: Nokia5110.h
+.\main.o: bitmapPhotos.h
+.\main.o: TivaES.h
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
